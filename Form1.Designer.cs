6e785167b05a4b95d67615ce2b2f72d0b7df14d1
Aliona Kavalenka <kovalenkoalena789@gmail.com>
2023-08-18 15:19:51 +0300

    Added screen catching

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,12 +28,79 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.captureButton = new System.Windows.Forms.Button();
+            this.recordButton = new System.Windows.Forms.Button();
+            this.stopButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.Location = new System.Drawing.Point(26, 63);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(749, 362);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // captureButton
+            // 
+            this.captureButton.Location = new System.Drawing.Point(26, 12);
+            this.captureButton.Name = "captureButton";
+            this.captureButton.Size = new System.Drawing.Size(118, 37);
+            this.captureButton.TabIndex = 1;
+            this.captureButton.Text = "Capture";
+            this.captureButton.UseVisualStyleBackColor = true;
+            this.captureButton.Click += new System.EventHandler(this.captureButton_Click);
+            // 
+            // recordButton
+            // 
+            this.recordButton.Image = ((System.Drawing.Image)(resources.GetObject("recordButton.Image")));
+            this.recordButton.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.recordButton.Location = new System.Drawing.Point(165, 12);
+            this.recordButton.Name = "recordButton";
+            this.recordButton.Size = new System.Drawing.Size(118, 37);
+            this.recordButton.TabIndex = 2;
+            this.recordButton.Text = "Record";
+            this.recordButton.UseVisualStyleBackColor = true;
+            this.recordButton.Click += new System.EventHandler(this.recordButton_Click);
+            // 
+            // stopButton
+            // 
+            this.stopButton.Location = new System.Drawing.Point(304, 12);
+            this.stopButton.Name = "stopButton";
+            this.stopButton.Size = new System.Drawing.Size(118, 37);
+            this.stopButton.TabIndex = 3;
+            this.stopButton.Text = "Stop";
+            this.stopButton.UseVisualStyleBackColor = true;
+            this.stopButton.Click += new System.EventHandler(this.stopButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.stopButton);
+            this.Controls.Add(this.recordButton);
+            this.Controls.Add(this.captureButton);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "screenR";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private PictureBox pictureBox1;
+        private Button captureButton;
+        private Button recordButton;
+        private Button stopButton;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -2,9 +2,46 @@ namespace screenR
 {
     public partial class Form1 : Form
     {
+        
+
         public Form1()
+        {            
+            InitializeComponent();            
+        }
+
+        private void recordButton_Click(object sender, EventArgs e)
         {
-            InitializeComponent();
+            Thread t = new Thread(Capture);
+            t.Start();
+        }
+
+        private void captureButton_Click(object sender, EventArgs e)
+        {
+            Bitmap bt = new Bitmap(Screen.PrimaryScreen.Bounds.Width, Screen.PrimaryScreen.Bounds.Height);
+
+            Graphics graf = Graphics.FromImage(bt);
+            graf.CopyFromScreen(0, 0, 0, 0, bt.Size);
+
+            pictureBox1.Image = bt;
+        }
+
+        void Capture()
+        {
+            while (true)
+            {
+                Bitmap bt = new Bitmap(Screen.PrimaryScreen.Bounds.Width, Screen.PrimaryScreen.Bounds.Height);
+
+                Graphics graf = Graphics.FromImage(bt);
+                graf.CopyFromScreen(0, 0, 0, 0, bt.Size);
+
+                pictureBox1.Image = bt;
+
+                Thread.Sleep(20);
+            }
+        }
+
+        private void stopButton_Click(object sender, EventArgs e)
+        {
         }
     }
 }
